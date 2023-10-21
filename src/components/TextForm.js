eb9352fb226acf26d63925674475359207d12006
khushi-2002 <khushiagarwal915@gmail.com>
2023-10-21 11:47:04 +0530

    Added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,23 +1,49 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
-    const handleUpClick = ()=>{
+    const handleUpClick = () => {
         let newText = text.toUpperCase()
         SetupText(newText)
     }
 
-    const  handleONChange= (event)=>{
+    const handleDownClick = () =>{
+        let newText = text.toLowerCase()
+        SetupText(newText)
+    }
+
+    const handleONChange = (event) => {
         console.log("On Change")
         SetupText(event.target.value)
     }
-    const [text, SetupText] = useState("Enter your Text here")
+
+    const handleClearClick = ()=>{
+        SetupText('')
+    }
+
+    const handleReverseClick = ()=>{
+        SetupText(text.split(' ').reverse().join(' '))
+    }
+    // const [text, SetupText] = useState("Enter your Text here")
+    const [text, SetupText] = useState("")
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-            <textarea className="form-control"  value={text} onChange={handleONChange}id="mybox" rows="8"></textarea>
-        </div>
-         <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
-        </div>
+        <>
+            <div className='container'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleONChange} id="mybox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className="btn btn-success mx-1" onClick={handleDownClick}>Convert to LowerCase</button>
+                <button className="btn btn-danger mx-1" onClick={handleClearClick}>Clear Text</button>
+                <button className="btn btn-secondary mx-1" onClick={handleReverseClick}>Reverse Text</button>
+            </div>
+            <div className="container my-3">
+                <h2>Your Text Summary</h2>
+                <p>Your Text have {text.split(' ').length} words and {text.length} characters</p>
+                <p>{text.split(' ').length * 0.008} minutes to read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
